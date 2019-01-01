5317d608dec82420056e0b232efcc1730020a2f6
ferenckata <katagyik@gmail.com>
2019-01-01 15:00:49 +0100

    proceed to show data on plot based, lots to do remains

--- src/main/java/userInterface/PlotStartPageController.java
+++ src/main/java/userInterface/PlotStartPageController.java
@@ -1,15 +1,25 @@
 package userInterface;
 
 import backend.Project;
+import backend.Task;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.chart.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class PlotStartPageController {
+public class PlotStartPageController implements Initializable {
 
     private Project project;
     // use a class for all cases where it wants to go back to the main screen
@@ -26,6 +36,10 @@ public class PlotStartPageController {
 
     @FXML
     private Button back;
+
+
+    @FXML
+    private TableView<backend.Task> taskTable;
 
     @FXML
     private TableColumn<?, ?> taskColumn;
@@ -49,10 +63,75 @@ public class PlotStartPageController {
     private AreaChart<?, ?> totalWorkload;
 
     @FXML
+    private CategoryAxis workloadXAxis;
+
+    @FXML
+    private NumberAxis workloiadYAxis;
+
+
+    @FXML
     private PieChart memberTimePie;
 
     @FXML
     private BarChart<?, ?> memberTimeBar;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+    public void initData(Project project){
+
+
+        this.project = project;
+
+        // set up Gantt Chart
+
+        yAxis.setLabel("Task");
+        xAxis.setLabel("Week");
+
+
+        XYChart.Series series1 = new XYChart.Series();
+        XYChart.Series series2 = new XYChart.Series();
+
+        // pull task name and start and end times
+
+        for (Task task:project.getTasks()) {
+
+            series1.getData().add(new XYChart.Data(task.getStartWeek(), task.getName()));
+            series2.getData().add(new XYChart.Data((task.getEndWeek()-task.getStartWeek()), task.getName()));
+
+        }
+
+        // plot data
+        ganttChart.getData().addAll(series1, series2);
+        // add data to the table's columns
+        taskColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
+        memberColumn.setCellValueFactory(new PropertyValueFactory<>("member"));
+
+        taskTable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        ObservableList<Task> currentTasks = FXCollections.observableArrayList();
+        for (Task task: project.getTasks()){
+            currentTasks.add(task);
+        }
+
+        taskTable.setItems(currentTasks);
+
+        // set up Total Workload Chart
+
+        workloadXAxis.setLabel("Task");
+        workloiadYAxis.setLabel("Week");
+
+        // TODO: calculate overlap : backend task
+
+        // ToDO: calculate time spent by member (%) to show on Pie Chart and the same as absolute value to show on Bar Plot
+
+
+    }
+
 
     @FXML
     void backBtnClicked(ActionEvent event) throws IOException {
