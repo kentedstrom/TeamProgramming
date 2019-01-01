b216dc5a4c8d076f767fe894bfe9e5d395aa195f
ferenckata <katagyik@gmail.com>
2019-01-01 14:27:19 +0100

    creating start page for new project and adding different plots

---
+++ src/main/java/userInterface/EscapeHatch.java
@@ -0,0 +1,28 @@
+package userInterface;
+
+import backend.Project;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class escapeHatch {
+
+    void backBtnClicked(ActionEvent event, Project project) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation((getClass().getResource("/ProjectOverview.fxml")));
+        Parent projectOverview = loader.load();
+
+        Scene projectOverviewScene = new Scene(projectOverview, 800,500);
+        ProjectOverviewController controller = loader.getController();
+
+        controller.initData(project);
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(projectOverviewScene);
+        window.show();
+    }
+}
